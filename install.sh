fc8be9ff6bc35b635b5b97e521450797e45e777a
arztz <arztz@gmail.com>
2021-01-31 15:36:26 +0700

    add

---
+++ install.sh
@@ -0,0 +1,14 @@
+sudo apt-get install -y zsh
+sudo apt-get install -y curl
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+#Set ZSH_THEME="powerlevel10k/powerlevel10k" in ~/.zshrc.
+
+
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git config --global user.email "arztz@gmail.com"
+git config --global user.name "arztz"
